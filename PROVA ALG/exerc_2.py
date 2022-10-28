fab98b2555f7104f84a931c63318c9046487170b
Alessandra Moreira <alessandra.moreira01@fatec.gov.sp.br>
2022-10-28 00:26:43 -0300

    edição questão 3 e adicionado questão 2

---
+++ PROVA ALG/exerc_2.py
@@ -0,0 +1,15 @@
+
+salario_fixo = int(input("Digite o salário fixo: R$"))
+valor_vendas = int(input("Digite o valor das vendas: R$"))
+comissao1 = 0.05
+comissao2 = 0.07
+
+if valor_vendas <= 1500:
+    comissao = valor_vendas * comissao1
+    salario_total = salario_fixo + comissao
+    print (f"O salário total é: R${salario_total}")
+
+elif valor_vendas > 1500:
+    comissao = valor_vendas * comissao2
+    salario_total = salario_fixo + comissao
+    print (f"O salário total é: R${salario_total}")
